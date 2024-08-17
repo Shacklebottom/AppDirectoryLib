76de86b401d87eed50fcc37956faf935924b06c4
Shacklebottom <last0illusion@gmail.com>
2024-08-17 17:43:21 -0500

    initial commit, that time I saw a real good breakpoint

---
+++ AppDirectory/AppDirectory.cs
@@ -0,0 +1,39 @@
+﻿
+namespace AppDirectoryModule
+{
+    public class AppDirectory : IAppDirectory
+    {
+        public string RootDirectory { get; private set; }
+
+        public AppDirectory(string rootDir)
+        {
+            RootDirectory = rootDir;
+
+            CreateAppDirectory(rootDir);
+        }
+
+        public void CreateAppDirectory(string path)
+        {
+            if (Directory.Exists(path)) return;
+
+            Directory.CreateDirectory(path);
+        }
+
+        public void CreateAppDirectories(string[] paths)
+        {
+            var currentDirectories = Directory.GetDirectories(RootDirectory);
+            var newDirectories = paths.Where(p => !currentDirectories.Contains(p));
+
+            if (!newDirectories.Any()) return;
+
+            foreach (string path in newDirectories)
+            {
+                Directory.CreateDirectory(path);
+            }
+        }
+        public IEnumerable<string> GetDirectoryFiles(string path)
+        {
+            return Directory.EnumerateFiles(path);
+        }
+    }
+}
---
+++ AppDirectory/IAppDirectory.cs
@@ -0,0 +1,12 @@
+﻿
+namespace AppDirectoryModule
+{
+    public interface IAppDirectory
+    {
+        string RootDirectory { get; }
+
+        void CreateAppDirectory(string path);
+
+        IEnumerable<string> GetDirectoryFiles(string path);
+    }
+}
